799d74dfbc63243c96cd9bcf2a9b96d4613005ed
Etienne Canuel <etienne.canuel@gmail.com>
2019-01-04 17:10:08 +0100

    maj

--- SwExtension/Macros/BoutonRetournerDvp.cs
+++ SwExtension/Macros/BoutonRetournerDvp.cs
@@ -21,12 +21,19 @@ namespace Macros
 
                 var ListeVue = MdlBase.eSelect_RecupererListeObjets<View>();
 
+                if (ListeVue.IsNull() || (ListeVue.Count == 0))
+                    ListeVue = MdlBase.eDrawingDoc().eFeuilleActive().eListeDesVues();
+
+                if (ListeVue.IsNull() || (ListeVue.Count == 0)) return;
+
                 MdlBase.eEffacerSelection();
 
                 foreach (var vue in ListeVue)
                 {
+                    if (vue.IsNull()) continue;
+
                     WindowLog.Ecrire(vue.Name);
-                    vue.FlipView = true;
+                    vue.FlipView = !vue.FlipView;
                 }
 
             }
